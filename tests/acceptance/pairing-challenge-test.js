723a2e46710906eaaafeecdeedc2c0b27c042f3c
rrglomsk <rachelglomski@eflexsystems.com>
2025-03-10 12:11:20 -0400

    wip

---
+++ tests/acceptance/pairing-challenge-test.js
@@ -0,0 +1,27 @@
+import { module, test } from 'qunit';
+import { visit, currentURL, click, fillIn } from '@ember/test-helpers';
+import { setupApplicationTest } from 'pairing-challenge/tests/helpers';
+
+module('Acceptance | pairing challenge', function (hooks) {
+  setupApplicationTest(hooks);
+
+  test('visiting /', async function (assert) {
+    await visit('/');
+
+    assert.strictEqual(currentURL(), '/');
+    assert.dom('h2').hasText('Inventory Tracker');
+  });
+
+  test('can add a new item', async function (assert) {
+    await visit('/');
+    assert.dom('.inventory-table tbody tr:last-child').containsText('4k TV');
+
+    await click('.add-item-button');
+    assert.dom('.add-item-modal').containsText('New Item');
+
+    await fillIn('.name-input input', 'Autographed Photo');
+    await click('.submit-button');
+
+    assert.dom('.inventory-table tbody tr:last-child').containsText('Autographed Photo');
+  });
+});
